a3ed2f755ecda7b2dd3e0800e37ec0767d200eda
dmitriigoi <goy.dmitriy@gmail.com>
2026-01-25 16:44:03 +0100

    Implement K8sSecretInitializer with multiple source support and updated configurations

--- src/main/java/com/example/secretexample/K8sSecretInitializer.java
+++ src/main/java/com/example/secretexample/K8sSecretInitializer.java
@@ -22,23 +22,39 @@ public class K8sSecretInitializer implements ApplicationContextInitializer<Confi
             Configuration.setDefaultApiClient(client);
             CoreV1Api api = new CoreV1Api();
 
-            String namespace = "secret-example";
-            // Use list as it worked in DebugController
-            V1SecretList list = api.listNamespacedSecret(namespace, null, null, null, null, null, null, null, null, null, null, null);
-            V1Secret appSecret = list.getItems().stream()
-                    .filter(s -> "secret-app".equals(s.getMetadata().getName()))
-                    .findFirst()
-                    .orElse(null);
-
-            if (appSecret != null && appSecret.getData() != null) {
-                Map<String, Object> properties = new HashMap<>();
-                appSecret.getData().forEach((k, v) -> properties.put(k, new String(v)));
+            int index = 0;
+            while (true) {
+                String nameKey = String.format("spring.cloud.kubernetes.secrets.sources[%d].name", index);
+                String namespaceKey = String.format("spring.cloud.kubernetes.secrets.sources[%d].namespace", index);
                 
-                MapPropertySource source = new MapPropertySource("manual-k8s-secret-source", properties);
-                applicationContext.getEnvironment().getPropertySources().addFirst(source);
-                System.out.println("[K8sSecretInitializer] Successfully loaded secret-app from API");
-            } else {
-                System.out.println("[K8sSecretInitializer] secret-app not found in namespace " + namespace);
+                String secretName = applicationContext.getEnvironment().getProperty(nameKey);
+                if (secretName == null) {
+                    break;
+                }
+                
+                String namespace = applicationContext.getEnvironment().getProperty(namespaceKey, "default");
+                
+                try {
+                    V1SecretList list = api.listNamespacedSecret(namespace, null, null, null, null, null, null, null, null, null, null, null);
+                    V1Secret appSecret = list.getItems().stream()
+                            .filter(s -> secretName.equals(s.getMetadata().getName()))
+                            .findFirst()
+                            .orElse(null);
+
+                    if (appSecret != null && appSecret.getData() != null) {
+                        Map<String, Object> properties = new HashMap<>();
+                        appSecret.getData().forEach((k, v) -> properties.put(k, new String(v)));
+                        
+                        MapPropertySource source = new MapPropertySource("manual-k8s-secret-source-" + secretName, properties);
+                        applicationContext.getEnvironment().getPropertySources().addFirst(source);
+                        System.out.println("[K8sSecretInitializer] Successfully loaded " + secretName + " from API in namespace " + namespace);
+                    } else {
+                        System.out.println("[K8sSecretInitializer] " + secretName + " not found in namespace " + namespace);
+                    }
+                } catch (Exception e) {
+                    System.err.println("[K8sSecretInitializer] Failed to load secret " + secretName + ": " + e.getMessage());
+                }
+                index++;
             }
         } catch (Exception e) {
             System.err.println("[K8sSecretInitializer] Failed to load secrets: " + e.getMessage());
